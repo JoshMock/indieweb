39815e1775a369327a15bc9d4ae8158674f4ba36
Josh Mock <josh@joshmock.com>
2021-11-24 13:07:24 -0600

    Quoteback support
    closes #22

--- .eleventy.js
+++ .eleventy.js
@@ -9,4 +9,6 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addFilter('simpleDate', dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
+
+  eleventyConfig.addPairedShortcode("quoteback", (text, author, title, url) => `<blockquote class="quoteback" darkmode="" data-title="${title}" data-author="${author}" cite="${url}"><div class="css-1dbjc4n"><div class="css-1dbjc4n r-1s2bzr4"><div dir="auto" class="css-901oao r-1fmj7o5 r-37j5jr r-1blvdjr r-16dba41 r-vrz42v r-bcqeeo r-bnwqim r-qvutc0" id="id__o67yoo50f0g" lang="en"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">${text}</span></div></div></div><footer>${author}</footer></blockquote>`);
 };
